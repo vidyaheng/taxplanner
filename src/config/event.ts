f1e1740e4accdf04afcfaf633e31cd964e2fb5e0
vidyaheng <vidyah@gmail.com>
2026-08-20 13:22:51 +0700

    ชื่อบริษัท

--- src/config/event.ts
+++ src/config/event.ts
@@ -1,5 +1,5 @@
 export const EVENT_CONFIG = {
-  company: "บริษัท A",
-  expires: "3 กันยายน 2569",
+  company: "บริษัท เหล็แผ่นรีดเย็นไทย จำกัด(มหาชน)",
+  expires: "4 กันยายน 2569",
   active: true,
 };
